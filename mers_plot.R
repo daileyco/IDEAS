ebfbacae7002df2ad592b287470182bfc7214818
Cody Aaron Dailey <daileyco@uga.edu>
2019-05-15 10:44:56 -0400

    comment out base plot

--- mers_plot.R
+++ mers_plot.R
@@ -1,6 +1,6 @@
 mers <- read.csv("cases.csv")
 
-hist(as.numeric(mers$age), xlab="Age", main="Histogram of Age in MERS Cases Dataset")
+#hist(as.numeric(mers$age), xlab="Age", main="Histogram of Age in MERS Cases Dataset")
 
 
 library(ggplot2)
