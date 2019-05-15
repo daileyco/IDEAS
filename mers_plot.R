3fe39c14d4e3c628f9fbb87312b33a027b5ced3d
Cody Aaron Dailey <daileyco@uga.edu>
2019-05-15 10:00:47 -0400

    added ggplot histogram

--- mers_plot.R
+++ mers_plot.R
@@ -1,3 +1,8 @@
 mers <- read.csv("cases.csv")
 
 hist(as.numeric(mers$age), xlab="Age", main="Histogram of Age in MERS Cases Dataset")
+
+
+library(ggplot2)
+
+ggplot(data=mers, mapping=aes(x=as.numeric(age)))+geom_histogram(binwidth = 5)+labs(x="Age", y="Frequency", title="Histogram of Age in MERS Cases Dataset", caption="Data from: https://github.com/rambaut/MERS-Cases/blob/gh-pages/data/cases.csv")
